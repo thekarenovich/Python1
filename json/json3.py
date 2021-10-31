7e58d8d44a4d610f7af551e6719ba3b3acdb5f34
Erik <78443570+thekhachik@users.noreply.github.com>
2021-10-31 03:08:55 +0300

    Create json3.py

---
+++ json/json3.py
@@ -0,0 +1,27 @@
+# Запись в файл JSON
+
+my_json_string = """{
+   "article": [
+      {
+         "id":"01",
+         "language": "JSON",
+         "edition": "first",
+         "author": "Derrick Mwiti"
+      },
+      {
+         "id":"02",
+         "language": "Python",
+         "edition": "second",
+         "author": "Derrick Mwiti"
+      }
+   ],
+   "blog":[
+   {
+       "name": "Datacamp",
+       "URL":"datacamp.com"
+   }
+   ]
+}
+"""
+with open('test_file.json', 'w') as file:
+    json.dump(my_json_string, file) # Метод
