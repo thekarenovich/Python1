f66d886984ccb8c03279608f6452a96a3d7e07c1
Erik <78443570+thekhachik@users.noreply.github.com>
2021-10-31 03:06:46 +0300

    Create json2.py

---
+++ json/json2.py
@@ -0,0 +1,18 @@
+# dict -> Object
+
+import json
+blog = {'URL': 'datacamp.com', 'name': 'Datacamp'} # Cловарь 
+to_json= json.dumps(blog) # Метод
+
+'''
+Python	JSON
+dict	Object
+list	Array
+tuple	Array
+str	String
+int	Number
+float	Number
+True	true
+False	false
+None	null
+'''
