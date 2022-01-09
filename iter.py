b8ea545f00786421004d71582ed5ee4e9a11581c
Erik <78443570+thekhachik@users.noreply.github.com>
2022-01-09 11:48:00 +0300

    Create iter.py

---
+++ iter.py
@@ -0,0 +1,14 @@
+mystr = "банан"
+myit =  iter(mystr) #переобразование строки(или можно списка, например) в итератор
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+
+# Вывод:
+# б
+# а
+# н
+# а
+# н
