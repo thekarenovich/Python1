3f3e31ad37adfc4801cccdffb7df695d53ba3cce
Erik <78443570+thekhachik@users.noreply.github.com>
2022-01-09 22:18:06 +0300

    Create random.py

---
+++ random.py
@@ -0,0 +1,11 @@
+import random
+
+a = 5
+b = 25
+l = [1,2,3,4,5,6,7,8,9,10]
+
+print(random.randint(a,b))
+# случайное число из интервала [a,b]
+
+print(random.choice(l))
+# случайный элемент из последовательности l
