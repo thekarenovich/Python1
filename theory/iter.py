e7f7016fcf1995c99ed9d1c62996ac1b0676673e
Erik <78443570+thekhachik@users.noreply.github.com>
2022-02-04 21:12:56 +0300

    Update iter.py

--- theory/iter.py
+++ theory/iter.py
@@ -1,14 +1,12 @@
-mystr = "банан"
+mystr = "fuck"
 myit =  iter(mystr) #переобразование строки(или можно списка, например) в итератор
-print(next(myit))
 print(next(myit))
 print(next(myit))
 print(next(myit))
 print(next(myit))
 
 # Вывод:
-# б
-# а
-# н
-# а
-# н
+# f
+# u
+# c
+# k
