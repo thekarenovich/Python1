bbf5615402b5a18bb1c306489703ffcde12c74e8
Erik <78443570+thekhachik@users.noreply.github.com>
2022-03-02 22:13:04 +0300

    Update arg.py

--- theory/arg.py
+++ theory/arg.py
@@ -1,5 +1,11 @@
+# Аргументы командной строки
 import sys
 
 if __name__ == "__main__":
     for param in sys.argv:
         print (param)
+     
+# Ввод: python3 file.py erik the best
+# Вывод: file.py erik the best
+
+# C argv можно работать, как со списком или строкой
