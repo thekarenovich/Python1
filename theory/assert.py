eb643933c7db3f5b70cd30bb0c37f098ee40fa8d
Erik <78443570+thekhachik@users.noreply.github.com>
2022-03-02 18:34:56 +0300

    Update assert.py

--- theory/assert.py
+++ theory/assert.py
@@ -8,3 +8,31 @@ print(z)
 
 a = divide(21,0)
 print(a)
+
+'''
+Метод	                     Проверка на	
+
+assertEqual(x, y)	         x == y	
+
+assertNotEqual(x, y)	     x != y	
+
+assertTrue(x)	             bool(x) равно True	
+
+assertFalse(x)	             bool(x) равно False	
+
+assertIs(x, y)	             x это y	
+
+assertIsNot(x, y)	         x это не y	
+
+assertIsNone(x)	             x это None	
+
+assertIsNotNone(x)	         x это не None	
+
+assertIn(x, y)	             x в y	
+
+assertNotIn(x, y)	         x не в y	
+
+assertIsInstance(x, y)	     isinstance(x, y)	
+
+assertNotIsInstance(x,y)	 не isinstance(x, y)	
+'''
