3e42b94302434bd97b7980d6972ea66bc081add6
Erik <78443570+thekhachik@users.noreply.github.com>
2022-03-04 00:03:50 +0300

    Delete 6.py

--- 6.py
+++
@@ -1,8 +0,0 @@
-# Реализуйте свою версию print(). Постарайтесь использовать максимум возможностей настоящей print().
-# Для вывода используйте функцию sys.stdout.write().
-
-import sys
-
-def print(*args):
-    sys.stdout.write(' '.join(map(str.upper, args)))
-print('asd', 'sad')
