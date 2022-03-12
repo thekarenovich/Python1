1f9a3e594e3bd048c33e76a76ed6730d19867c3c
Erik <78443570+thekhachik@users.noreply.github.com>
2022-03-12 14:23:26 +0300

    Add files via upload

---
+++ tasks/second_year/1.py
@@ -0,0 +1,26 @@
+from gevent import monkey; monkey.patch_all()
+
+def add_function(a, b):
+    c = str(a) + b
+    print "c is %s" % c
+
+def add_int_function(c, d):
+    e = c + d
+    print "the vaule of e is %d" % e
+
+def func(key1 = 'val1',
+         key2 = 'val2'):
+    return key1, key2
+
+if __name__ =="__main__":
+    var=8
+    add_function(59906,'kugrt5')
+    print ( 5 )
+    if x > 5: y = 10
+    if var != True:
+        print("var is not equal to True")
+    if var == None:
+        print("var is equal to None")
+
+
+# http://pep8online.com/
