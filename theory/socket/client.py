d145e604dd6c1fdca8ff9afc35d3aa2f2e0d6ca2
Erik <78443570+thekhachik@users.noreply.github.com>
2022-04-27 02:27:33 +0300

    Update client.py

--- theory/socket/client.py
+++ theory/socket/client.py
@@ -1,7 +1,7 @@
 import socket 
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.bind(('127.0.0.1', 12345))
+client.connect(('127.0.0.1', 12345))
 
 while True:
 	data = client.recv(1024)
