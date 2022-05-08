52608c8747f1d24fccef5f662d74439bccd12de7
Erik <78443570+thekhachik@users.noreply.github.com>
2022-05-09 01:25:01 +0300

    Add files via upload

---
+++ theory/length_str.py
@@ -0,0 +1,31 @@
+# Максимальная длина строки
+# Ограничьте длину строки максимум 79 символами.
+# Для более длинных блоков текста с меньшими структурными ограничениями 
+# (строки документации или комментарии), 
+# длину строки следует ограничить 72 символами.
+
+
+class Rectangle(Blob):
+
+    def __init__(self, width, height,
+                 color='black', emphasis=None, highlight=0):
+        if (width == 0 and height == 0 and
+                color == 'red' and emphasis == 'strong' or
+                highlight > 100):
+            raise ValueError("sorry, you lose")
+        if width == 0 and height == 0 and (color == 'red' or
+                                           emphasis is None):
+            raise ValueError("I don't think so -- values are %s, %s" %
+                             (width, height))
+        Blob.__init__(self, width, height,
+                      color, emphasis, highlight)
+
+
+with open('/path/to/some/file/you/want/to/read') as file_1, \
+        open('/path/to/some/file/being/written', 'w') as file_2:
+    file_2.write(file_1.read())
+
+
+# Данный скрипт запускать не надо!
+
+
