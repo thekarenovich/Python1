3f91518d68561a135fac739f6fa9d9ec7bb6b9bc
Erik Karenovich <78443570+thekarenovich@users.noreply.github.com>
2022-10-24 01:05:27 +0300

    Update 2.py

--- tasks/Interview/dict/2.py
+++ tasks/Interview/dict/2.py
@@ -11,3 +11,4 @@ biggest_dict(k1=22, k2=31, k3=11, k4=91)
 biggest_dict(name='Елена', age=31, weight=61, eyes_color='grey')
 print(my_dict)  
 # {'first_one': 'we can do it', 'k1': 22, 'k2': 31, 'k3': 11, 'k4': 91, 'name': 'Елена', 'age': 31, 'weight': 61, 'eyes_color': 'grey'}
+
