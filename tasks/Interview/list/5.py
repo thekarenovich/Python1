a368f15ce447b8d15fd2715619559e7a508efe9d
Erik Karenovich <78443570+thekarenovich@users.noreply.github.com>
2022-10-23 15:35:48 +0300

    Update 5.py

--- tasks/Interview/list/5.py
+++ tasks/Interview/list/5.py
@@ -4,6 +4,7 @@ def list_sort(lst):
     lst.sort(key=lambda x: abs(x), reverse=True)
     return lst
 
+
 print(list_sort([1, 5, 77]))  # [77, 5, 1]
 print(list_sort([19, 8.3, -4, 11, 0, 5]))  # [19, 11, 8.3, 5, -4, 0]
 print(list_sort([-33, -0.05, -4.18, 11.2, 13.12, 55, 7.1]))  # [55, -33, 13.12, 11.2, 7.1, -4.18, -0.05]
