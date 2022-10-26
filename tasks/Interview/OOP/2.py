49320f64909b6116dec4ca94a3287cfb1e0366cc
Erik Karenovich <78443570+thekarenovich@users.noreply.github.com>
2022-10-26 21:24:41 +0300

    Create 2.py

---
+++ tasks/Interview/OOP/2.py
@@ -0,0 +1,36 @@
+# Николаю требуется проверить, возможно ли из представленных отрезков условной длины сформировать треугольник. 
+# Для этого он решил создать класс TriangleChecker, принимающий только положительные числа. 
+# С помощью метода is_triangle() возвращаются следующие значения (в зависимости от ситуации):
+# – Ура, можно построить треугольник!;
+# – С отрицательными числами ничего не выйдет!;
+# – Нужно вводить только числа!;
+# – Жаль, но из этого треугольник не сделать.
+
+class TriangleChecker:
+    def __init__(self, sides):
+        self.sides = sides
+
+    def is_triangle(self):
+        if all(isinstance(side, (int, float)) for side in self.sides):
+            if all(side > 0 for side in self.sides):
+                sorted_sides = sorted(self.sides)
+                if sorted_sides[0] + sorted_sides[1] > sorted_sides[2]:
+                    return 'Ура, можно построить треугольник!'
+                return 'Жаль, но из этого треугольник не сделать'
+            return 'С отрицательными числами ничего не выйдет!'
+        return 'Нужно вводить только числа!'
+ 
+ 
+triangle1 = TriangleChecker([2, 3, 4])
+print(triangle1.is_triangle())
+triangle2 = TriangleChecker([77, 3, 4])
+print(triangle2.is_triangle())
+triangle3 = TriangleChecker([77, 3, 'Сторона3'])
+print(triangle3.is_triangle())
+triangle4 = TriangleChecker([77, -3, 4])
+print(triangle4.is_triangle())
+
+# Ура, можно построить треугольник!
+# Жаль, но из этого треугольник не сделать
+# Нужно вводить только числа!
+# С отрицательными числами ничего не выйдет!
