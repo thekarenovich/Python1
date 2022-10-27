20aeab08bad61fddaab8dd278e4aacf6e9e69ebe
Erik Karenovich <78443570+thekarenovich@users.noreply.github.com>
2022-10-28 00:45:12 +0300

    Update 5.py

--- tasks/Interview/files/5.py
+++ tasks/Interview/files/5.py
@@ -17,7 +17,7 @@ def circles_generator(num_of_circles=100):
         draw.ellipse((0, 0, 600, 600), fill=(randint(0, 255), randint(0, 255), randint(0, 255)))
         img.save(f'circles/{pic_name}.jpg', quality=85)
  
- 
+
 circles_generator()
 
 # В результате выполнения скрипта в папке circles появится 100 картинок кругов одинакового размера.
