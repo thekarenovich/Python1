04fdf871462001fb9d78d3236819937fea9fe885
Erik Karenovich <78443570+thekarenovich@users.noreply.github.com>
2022-12-29 16:10:28 +0300

    Create join_and_split.py

---
+++ theory/str/join_and_split.py
@@ -0,0 +1,15 @@
+# split()
+my_string = "I code for 2 hours everyday"
+my_string.split()
+# ['I', 'code', 'for', '2', 'hours', 'everyday']
+
+my_string = "Apples,Oranges,Pears,Bananas,Berries"
+my_string.split(",")
+# ['Apples', 'Oranges', 'Pears', 'Bananas', 'Berries']
+
+# join()
+", ".join(['Apples', 'Oranges', 'Pears', 'Bananas', 'Berries'])
+# Apples, Oranges, Pears, Bananas, Berries
+
+"___".join(['Apples', 'Oranges', 'Pears', 'Bananas', 'Berries'])
+# Output: Apples___Oranges___Pears___Bananas___Berries
