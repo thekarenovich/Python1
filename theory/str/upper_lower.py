731d25569351f8c3140f3469524c563e0aa56ac0
Erik Karenovich <78443570+thekarenovich@users.noreply.github.com>
2022-12-29 16:25:48 +0300

    Create upper_lower.py

---
+++ theory/str/upper_lower.py
@@ -0,0 +1,11 @@
+print('AjfjBHDaJaL'.swapcase())
+print('AjfjBHDaJaL'.upper())
+print('AjfjBHDaJaL'.lower())
+print('ADBCHDBCHD'.isupper())
+print('Ajfja'.islower())
+
+# aJFJbhdAjAl
+# AJFJBHDAJAL
+# ajfjbhdajal
+# True
+# False
