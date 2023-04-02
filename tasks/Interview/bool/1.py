5c8a6e58fe290282011fe5f4e0a76b8b16f3e77f
Erik Karenovich <78443570+thekarenovich@users.noreply.github.com>
2023-04-03 00:32:47 +0300

    Update 1.py

--- tasks/Interview/bool/1.py
+++ tasks/Interview/bool/1.py
@@ -7,7 +7,7 @@
 def dislike_6(a):
     if (type(a) is float or type(a) is int) and a == 6.0:
         return 'Только не 6!' 
-    return True
+    return True 
 
 print(dislike_6(6.0))  # Только не 6!
 print(dislike_6(6))  # Только не 6!
